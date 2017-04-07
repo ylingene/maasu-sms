4a62bd28d616a84aed65a5c0e8c2c61a81e94c81
Kelvin Tam <kelvin.tam@live.com>
2017-04-07 02:48:42 -0400

    ksejrkejrk

--- app.py
+++ app.py
@@ -14,26 +14,29 @@ def sms():
 	message_body = message_body.lower().replace(" ", "").translate(None, string.punctuation)
 
 	if message_body == "banquet":
-		message = "Saturday's banquet will be held at The Diamond Center of Novi (46100 Grand River Ave, Novi, MI 48374). Food and entertainment will be provided. Buses for banquet (if requested) will be leaving at 6:30PM. Reply 'bus' for more bus information."
+		message = "Saturday's banquet will be held at The Diamond Center of Novi (46100 Grand River Ave, Novi, MI 48374). Food and entertainment will be provided. Buses for banquet (if requested) will be leaving at 6:30PM. Reply 'bus' for more bus information.";
 	elif message_body == "friday":
-		message = ""
-	elif message_body == "easthall":
-		message = "easthall"
-	elif message_body == "masonhall":
-		message = "masonhall"
+		message = "We have two events for you on Friday Night: Spring Into MAASU and GenAPA. Reply 'spring' or 'genapa' for more info.";
+	elif message_body == "east":
+		message = "East Hall is where our Friday night entertainment, Spring Into MAASU, is located. Address: 530 Church St, Ann Arbor, MI 48109";
+	elif message_body == "mason":
+		message = "Mason Hall will be where all workshops and small group sessions are located. Address: 419 S State St Mason Hall, Ann Arbor, MI 48109";
 	elif message_body == "rackham":
-		message = "rackham"
-	elif message_body == "united":
+		message = "Rackham Auditorium will house our opening ceremony as well as Saturday morning registration. Address: 915 E Washington St, Ann Arbor, MI 48109";
+	elif message_body == "maasu":
 		message = """Here are the commands I accept: 
+					\n'parking': parking information
 					\n'friday': friday night information
 					\n'banquet': banquet information
-					\n'east hall': address for East Hall (Friday night entertainment and Friday registration)
-					\n'mason hall': address for Mason Hall (all workshops)
+					\n'east': address for East Hall (Friday night entertainment and Friday registration)
+					\n'mason': address for Mason Hall (all workshops)
 					\n'rackham': address for Rackham Auditorium (Opening ceremony and Saturday registration)
 					\n'bus': bus information
 		"""
 	elif message_body == "bus":
-		message = "bus"
+		message = "Buses will begin boarding at 6PM and will depart at 6:30PM. Pickup location is on the corner of N University Ave. and S State St.";
+	elif message_body == "parking":
+		message = "Parking will be $10 for the day at the Thayer Parking Structure. Address: 216 South Thayer Street, Ann Arbor, MI 48104";
 	else:
 		message = """Here are the commands I accept: 
 					\n'friday': friday night information
